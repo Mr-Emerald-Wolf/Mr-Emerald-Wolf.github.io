ba9d73629cf967fd52aa38aa96a4e9cd5c4a6563
Mr-Emerald-Wolf <callofdutyuss@gmail.com>
2019-09-12 01:51:00 +0530

    Footer added but not customized.

--- index.php
+++ index.php
@@ -52,91 +52,98 @@
         <h1 class="text-center display-4 mb-4">Posts</h1><br>
         <div class="container">
           <div class="row w-100 mt-4 mx-auto">
-<?php 
-    for ($x = 0; $x <= 5; $x++) {
+            <?php 
+              for ($x = 0; $x <= 5; $x++) {
         
-        require_once('post_config.php');
-            $sql = "SELECT * FROM `express-posts` ORDER BY `express-posts`.`id` DESC LIMIT ". $x. ",1"; 
-            
-            $result = $conn->query($sql);
-            $row = $result->fetch_assoc();
-        $path = $row['path'];
-        echo '  <div class="col-md-4 mt-3 mb-3">';
+                require_once('post_config.php');
+                $sql = "SELECT * FROM `express-posts` ORDER BY `express-posts`.`id` DESC LIMIT ". $x. ",1"; 
+                $result = $conn->query($sql);
+                $row = $result->fetch_assoc();
+                $path = $row['path'];
+                    
+                echo '  <div class="col-md-4 mt-3 mb-3">';
         if ($x >= 3) {
-            echo '<div class="collapse multi-collapse" id="multiCollapseExample1">';
+                echo '<div class="collapse multi-collapse" id="multiCollapseExample1">';
         }
-        echo '      <div class="card h-100">';
-        echo '<a href="post.php?id='. $path. ' " class="stretched-link"></a>';
+                echo '      <div class="card h-100">';
+                echo '<a href="post.php?id='. $path. ' " class="stretched-link"></a>';
         
-    echo "<img src=' " .$row['img1'] . "' class='card-img-top rounded' alt='Responsive image'>";                 
+                echo "<img src=' " .$row['img1'] . "' class='card-img-top rounded' alt='Responsive image'>";                 
+                  
                   echo '<div class="card-body">';
                   echo '<h5 class="card-title">'. $row['name']. '</h5>';
                   echo '<p class="card-text truncate">'. $row['sub_heading']. '</p>';
                   echo '</div>';
 
-        echo '      </div>';
+                echo '      </div>';
         if ($x >=3) {
-            echo '  </div>';
+                  echo '  </div>';
         }
-        echo '</div>';
+                echo '</div>';
 } 
-?>  
-              <div class="col text-center">                
-                  <button class="btn btn-light p-4 px-4 bg-white border" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Load More</button>
-              </div>
+?>
+            <div class="col text-center">
+              <button class="btn btn-light p-4 px-4 bg-white border" type="button" data-toggle="collapse" data-target=".multi-collapse" aria-expanded="false" aria-controls="multiCollapseExample1 multiCollapseExample2">Load More</button>
+            </div>
 
-    </div>
+          </div>
         </div>
       </div>
     </div>
     <div class="container-fliud min-100 d-flex flex-column">
-    <div class="row mx-auto h-50">    
+      <div class="row mx-auto h-50">
         <div class="container-fliud">
-            
-           <div class="row mx-auto"></div>
-                 <h1 class="display-4 text-center mb-3">Youtube</h1>
-            <div class="container">
-                <!-- Place <div> tag where you want the feed to appear -->
-<div id="curator-feed-default-layout"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
-<!-- The Javascript can be moved to the end of the html page before the </body> tag -->
-<script type="text/javascript">
-/* curator-feed-default-layout */
-(function(){
-var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
-i.src = "https://cdn.curator.io/published/fcf75152-3d36-48b4-abc1-9e3018f0c8be.js";
-e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-})();
-</script>
-            </div>
-            </div>
-        
-    </div>
-    <div class="row mx-auto h-50">
-           <div class="container">
-                 <h1 class="display-4 text-center mb-3">Instagram</h1>
-               </div>
-            <div class="container">
-              <!-- Place <div> tag where you want the feed to appear -->
-<div id="curator-feed-instagram-layout"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
-<!-- The Javascript can be moved to the end of the html page before the </body> tag -->
-<script type="text/javascript">
-/* curator-feed-instagram-layout */
-(function(){
-var i, e, d = document, s = "script";i = d.createElement("script");i.async = 1;
-i.src = "https://cdn.curator.io/published/85ba7ecc-b744-4986-a9f0-3319d4053024.js";
-e = d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
-})();
-</script>
-            </div>
-            
-            
-    </div>
-    <div class="container-fluid">
-        <h1 class="display-4 text-left">Contact Us</h1>
-        
-    </div>
-     
+
+          <div class="row mx-auto"></div>
+          <h1 class="display-4 text-center mb-3">Youtube</h1>
+          <div class="container">
+            <!-- Place <div> tag where you want the feed to appear -->
+            <div id="curator-feed-default-layout"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
+            <!-- The Javascript can be moved to the end of the html page before the </body> tag -->
+            <script type="text/javascript">
+              /* curator-feed-default-layout */
+              (function() {
+                var i, e, d = document,
+                  s = "script";
+                i = d.createElement("script");
+                i.async = 1;
+                i.src = "https://cdn.curator.io/published/fcf75152-3d36-48b4-abc1-9e3018f0c8be.js";
+                e = d.getElementsByTagName(s)[0];
+                e.parentNode.insertBefore(i, e);
+              })();
+
+            </script>
+          </div>
+        </div>
+
       </div>
+      <div class="row mx-auto h-50">
+        <div class="container">
+          <h1 class="display-4 text-center mb-3">Instagram</h1>
+        </div>
+        <div class="container">
+          <!-- Place <div> tag where you want the feed to appear -->
+          <div id="curator-feed-instagram-layout"><a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a></div>
+          <!-- The Javascript can be moved to the end of the html page before the </body> tag -->
+          <script type="text/javascript">
+            /* curator-feed-instagram-layout */
+            (function() {
+              var i, e, d = document,
+                s = "script";
+              i = d.createElement("script");
+              i.async = 1;
+              i.src = "https://cdn.curator.io/published/85ba7ecc-b744-4986-a9f0-3319d4053024.js";
+              e = d.getElementsByTagName(s)[0];
+              e.parentNode.insertBefore(i, e);
+            })();
+
+          </script>
+        </div>
+
+
+      </div>
+    </div>
+         <?php echo file_get_contents("footer.html"); ?>
   </body>
 
 </html>
