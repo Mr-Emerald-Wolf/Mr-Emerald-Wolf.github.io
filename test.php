efbcfe262614dd978e19bac48edb2d029519e476
BuildTools <unconfigured@null.spigotmc.org>
2019-10-29 00:36:09 +0530

    test.php created

---
+++ test.php
@@ -0,0 +1,17 @@
+<!---Use this as a testing ground to set up new resources--->
+<html lang="en">
+<head>
+<?php echo "<title>". $row['name']. " | ThinkYourSelfNow </title>"; ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/master.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <?php require_once('post_config.php'); ?>
+  </head>
+    <body>
+    
+    </body>
+</html>
