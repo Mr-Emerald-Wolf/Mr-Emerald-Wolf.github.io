c549333ef4f509bab89d92874123f80209be0446
Mr-Emerald-Wolf <callofdutyuss@gmail.com>
2019-11-17 22:50:32 +0530

    Login Page Created

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
         <title>Login | ThinkYourSelfNow </title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../css/master.css">
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/master.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -13,10 +13,7 @@
     <body>
         <style> .font-weight-normal{ font-weight: 400!important; }</style>
         <style> body{background-color: whitesmoke;}</style>
-         <?php echo file_get_contents("navbar.html"); ?>
-         <!--This calls the code for the navbar-->
-
-        
+    
         <div class="container text-center mt-5 h-100">
             <div class="row mx-auto py-2">
                 <div class="form-group mx-auto max-vw-50">
