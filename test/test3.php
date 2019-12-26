76504abc4c70491fa1f5a2fa8b1a87430ea1eda8
Mr-Emerald-Wolf <callofdutyuss@gmail.com>
2019-12-26 21:02:44 +0530

    Adding user Shivam

--- test/test3.php
+++ test/test3.php
@@ -1,4 +1,9 @@
 <?php 
-    session_start();
-    echo $_SESSION['user'];
-    ?>
+session_start();
+$pass = password_hasH('TDR4fury', PASSWORD_DEFAULT);
+if (password_verify('TDR4fury', $pass)) {
+	echo $pass;
+}
+echo $pass;
+
+?>
